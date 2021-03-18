6b996a07e3a4909da77c0211409bc7a8d8982d97
Jecesario <jp@soloid.com.br>
2021-03-17 21:27:27 -0300

    feat(components): criado component entrar e cadastrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntrarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule
